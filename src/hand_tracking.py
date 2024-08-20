bd9c50e53a61005e2c110afc540380dd95c8b73b
axelalcaraz3998 <axel.informatica.est22@gmail.com>
2024-08-19 19:30:53 -0600

    Draw line from tip of middle finger

--- src/hand_tracking.py
+++ src/hand_tracking.py
@@ -20,21 +20,24 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Results dictionary
 results = {
-  "hand_orientation": None,
+  "handedness": None,
   "hand_landmarks": None,
+  "hand_orientation": None,
 }
 
 # Callback function
 def result_callback(result: HandLandmarkerResult, output_image: mp.Image, timestamp_ms: int):
   if (len(result.handedness) > 0):
-    results["hand_orientation"] = result.handedness[0][0].display_name
+    results["handedness"] = result.handedness[0][0].display_name
   else:
+    results["handedness"] = None
     results["hand_orientation"] = None
 
   if (len(result.hand_landmarks) > 0):
     results["hand_landmarks"] = result.hand_landmarks[0]
   else:
     results["hand_landmarks"] = None
+    results["hand_orientation"] = None
 
 # Hand landmarker options
 options = HandLandmarkerOptions(
@@ -97,9 +100,14 @@ with HandLandmarker.create_from_options(options) as landmarker:
       coords = f"({(results["hand_landmarks"][0].x + results["hand_landmarks"][9].x) / 2}, {(results["hand_landmarks"][0].y + results["hand_landmarks"][9].y) / 2})"
       cv.putText(frame, coords, (0, 30), cv.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0), 1)
 
-      if results["hand_orientation"]:
-        # Write hand orientation
-        cv.putText(frame, results["hand_orientation"], (rect_right_bound, (rect_upper_bound - 10)), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
+      # Get hand orientation
+      line_x = int((int(results["hand_landmarks"][11].x * width) + int(results["hand_landmarks"][12].x * width)) / 2)
+      line_y = int(results["hand_landmarks"][12].y *  height)
+      cv.line(frame, (line_x, line_y), (line_x, 0), (0, 255, 0), 2)
+
+      if results["handedness"]:
+        # Write handedness
+        cv.putText(frame, results["handedness"], (rect_right_bound, (rect_upper_bound - 10)), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 255, 0), 2)
 
       for landmark in results["hand_landmarks"]:
         # Convert normalized coordinates to pixels in frame
