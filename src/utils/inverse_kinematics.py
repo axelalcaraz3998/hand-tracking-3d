9e3f0e7c1bf9abd2dda4ffedfade363937fa9792
axelalcaraz3998 <axel.informatica.est22@gmail.com>
2024-11-09 14:38:28 -0600

    Normalized angles and hand coordinates

--- src/utils/inverse_kinematics.py
+++ src/utils/inverse_kinematics.py
@@ -21,6 +21,10 @@ joint_3_obj = sim.getObject("./joint_3")
 joint_4_obj = sim.getObject("./joint_4")
 joint_5_obj = sim.getObject("./joint_5")
 
+# Helper function to normalize coordinates
+def clamp(value, min_val, max_val):
+  return max(min_val, min(value, max_val))
+
 def inverse_kinematics(coords, orientation):
   # Get coordinates
   x = coords[1]
@@ -28,28 +32,18 @@ def inverse_kinematics(coords, orientation):
   z = coords[2]
 
   # Get hand orientation
-  alpha = orientation[1]
-  miu = orientation[0]
+  alpha = orientation[0]
+  miu = orientation[1]
   phi = orientation[2]
   
   # Normalize coordinates
-  x = -x
-  x += 25
-  if x > 25:
-    x = 25
-  elif x < 0:
-    x = 0
+  x = -x + 25
+  x = clamp(x, 0, 25)
 
-  if y > 25:
-    y = 25
-  elif y < -25:
-    y = -25
+  y = clamp(y, -25, 25)
 
-  z -= 30
-  if z > 25:
-    z = 25
-  elif z < 0:
-    z = 0
+  z -= 50
+  z = clamp(z, 0, 25)
 
   # Joint 0
   theta[0] = np.atan2(y, x)
@@ -58,29 +52,27 @@ def inverse_kinematics(coords, orientation):
   # Joint 1
   theta[1] = np.atan2((z - d_1), (np.sqrt(x**2 + y**2))) - np.atan2((d_4*np.sin(theta[2])), (a_2 + d_4*np.cos(theta[2])))
 
-  # Rotation matrices
-  R1 = [[np.cos(theta[0]), 0, np.sin(theta[0])], [np.sin(theta[0]), 0, -np.cos(theta[0])], [0, 1, 0]]
-  R2 = [[np.cos(theta[1]), -np.sin(theta[1]), 0], [np.sin(theta[1]), np.cos(theta[1]), 0], [0, 0, 1]]
-  R3 = [[np.cos(theta[2]), 0, np.sin(theta[2])], [np.sin(theta[2]), 0, -np.cos(theta[2])], [0, 1, 0]]
-  R03 = np.matmul(R1, np.matmul(R2, R3))
+  # Normalize angles
+  theta[1] = clamp(theta[1], -3*(np.pi * 0.25), 3*(np.pi * 0.25))
+  theta[2] = clamp(theta[2], -3*(np.pi * 0.25), 3*(np.pi * 0.25))
 
-  alpha = alpha + (np.pi * 0.5)
+  # Orientation offsets
+  alpha = alpha - (theta[1] + theta[3])
+  phi = phi - theta[0]
 
-  # Orientation matrices
-  RA = [[np.cos(phi), 0, -np.sin(phi)], [np.sin(phi), 0, np.cos(phi)], [0, -1, 0]]
-  RB = [[np.cos(alpha), 0, np.sin(alpha)], [np.sin(alpha), 0, -np.cos(alpha)], [0, 1, 0]]
-  RC = [[np.cos(miu), -np.sin(miu), 0], [np.sin(miu), np.cos(miu), 0], [0, 0, 1]]
-  RT = np.matmul(RA, np.matmul(RB, RC))
-  R36 = np.matmul(np.transpose(R03), RT)
+  # Rotation coordinates
+  x2 = np.cos(alpha) * np.cos(phi)
+  y2 = np.sin(alpha) * np.cos(phi)
+  z2 = np.sin(phi)
 
   # Joint 3
-  theta[3] = np.atan2(R36[1, 2], R36[0, 2])
+  theta[3] = np.atan(y2 / z2)
   # Joint 4
-  theta[4] = np.atan2(np.sqrt(R36[0, 2]**2 + R36[1, 2]**2), R36[2, 2])
+  theta[4] = -np.atan(z2 / (np.sqrt(x2/y2)))
   # Joint 5
-  theta[5] = np.atan2(R36[2, 1], -R36[2, 0])
+  theta[5] = -theta[3] + miu
 
-  sim.setJointTargetPosition(joint_5_obj, theta[5] + 0)
+  sim.setJointTargetPosition(joint_5_obj, theta[5] + (np.pi * 0.5))
   sim.setJointTargetPosition(joint_4_obj, theta[4] + 0)
   sim.setJointTargetPosition(joint_3_obj, theta[3] + 0)
   sim.setJointTargetPosition(joint_2_obj, theta[2] + 3*(np.pi * 0.25))
